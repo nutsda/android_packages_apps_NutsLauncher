LawnchairLauncher/lawnchair

Stop using deprecated Canvas APIs.
Bug: 73777445
Test: compile
Change-Id: Ic0a1f4ebc10ba46229fd97408c052c0b450b4945

src/com/android/launcher3/graphics/LauncherIcons.java
@@ -302,7 +302,7 @@ public class LauncherIcons implements AutoCloseable {
         } else {
             icon.setBounds(left, top, left+width, top+height);
         }
-        mCanvas.save(Canvas.MATRIX_SAVE_FLAG);
+        mCanvas.save();
         mCanvas.scale(scale, scale, textureWidth / 2, textureHeight / 2);
         icon.draw(mCanvas);
         mCanvas.restore();

nutsda/android_packages_apps_NutsLauncher

(cherry picked from commit 26a1b452f47f9fe4f1cfc700d98df8a3a569eac2)

src/com/android/launcher3/graphics/LauncherIcons.java
@@ -264,7 +264,7 @@ public class LauncherIcons {
             } else {
                 icon.setBounds(left, top, left+width, top+height);
             }
-            canvas.save(Canvas.MATRIX_SAVE_FLAG);
+            canvas.save();
             canvas.scale(scale, scale, textureWidth / 2, textureHeight / 2);
             icon.draw(canvas);
             canvas.restore();
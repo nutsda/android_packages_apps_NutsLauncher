omnirom/android_packages_apps_Launcher3

Launcher3: add config to show notification dot numbers
Change-Id: Ief211a5f36b3b21e04a68da9457986ec20432f58

src/com/android/launcher3/Utilities.java
@@ -142,6 +142,7 @@ public final class Utilities {
     public static final String SHOW_EVENTS_PREFERENCE_KEY = "pref_showEvents";
     public static final String SHOW_LEFT_TAB_PREFERENCE_KEY = "pref_left_tab";
     public static final String SHOW_DARK_THEME = "pref_showDarkTheme";
+    public static final String SHOW_NOTIFICATION_DOT_NUMBERS = "pref_icon_badging_numbers";
 
     public static boolean isPropertyEnabled(String propertyName) {
         return Log.isLoggable(propertyName, Log.VERBOSE);
@@ -229,6 +230,10 @@ public final class Utilities {
 
     public static boolean isDarkTheme(Context context) {
         return getPrefs(context).getBoolean(SHOW_DARK_THEME, false);
+    }
+
+    public static boolean isShowNotificationDotNumbers(Context context) {
+        return getPrefs(context).getBoolean(SHOW_NOTIFICATION_DOT_NUMBERS, true);
     }
 
     /**

nutsda/android_packages_apps_NutsLauncher

Adapted to suit
Original commit https://github.com/omnirom/android_packages_apps_Launcher3/commit/02611eab0f7061cce5900146410c820a277d15a7
(cherry picked from commit 02611eab0f7061cce5900146410c820a277d15a7)

src/com/android/launcher3/Utilities.java
@@ -127,6 +127,7 @@ public final class Utilities {
             TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
 
     public static final String ALLOW_ROTATION_PREFERENCE_KEY = "pref_allowRotation";
+    public static final String SHOW_NOTIFICATION_DOT_COUNT_PREFERENCE_KEY = "pref_icon_badging_numbers";
 
     public static boolean isPropertyEnabled(String propertyName) {
         return Log.isLoggable(propertyName, Log.VERBOSE);
@@ -147,6 +148,10 @@ public final class Utilities {
             return originalSmallestWidth >= 600;
         }
         return false;
+    }
+
+    public static boolean isShowNotificationDotCount(Context context) {
+	return getPrefs(context).getBoolean(SHOW_NOTIFICATION_DOT_COUNT_PREFERENCE_KEY, true);
     }
 
     /**